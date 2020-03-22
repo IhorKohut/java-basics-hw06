80a5c723d025e063207f01ff5018b6b239bffeb1
IhorKohut <ih.kohut@gmail.com>
2020-03-22 22:59:25 +0200

    homework 06

--- src/Temperature.java
+++ src/Temperature.java
@@ -7,8 +7,8 @@ public class Temperature {
    * @return temperature degree in Celsius
    */
   public static double fahrenheitToCelsius(double fahrenheit) {
-    // TODO fill in code here and replace the return statement
-    return 0;
+    double celsius = (5 * (fahrenheit - 32) / 9);
+    return celsius;
   }
 
   /**
@@ -18,7 +18,7 @@ public class Temperature {
    * @return temperature degree in Fahrenheit
    */
   public static double celsiusToFahrenheit(double celsius) {
-    // TODO fill in code here and replace the return statement
-    return 0;
+    double fahrenheit = ((9 * celsius + 160) / 5);
+    return fahrenheit;
   }
 }
--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
-        // TODO add 2 more test data here
+        {-15, -26.11111111111111},
+        {320.0, 160.0},
+        {97.88000000000001, 36.6}
     };
   }
 
@@ -28,8 +29,9 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
-        // TODO add 2 more test data here
+        {-26.11111111111111, -15},
+        {160.0, 320.0},
+        {36.6, 97.88000000000001}
     };
   }
 
